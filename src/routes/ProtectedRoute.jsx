f574d1485aec2effa528c276e135a774c0b49a23
KawshikBss <kawshikbss@gmail.com>
2026-06-19 23:37:13 +0600

    fixed

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("api-token");
-    if (!token) return <Navigate to="/login" replace />;
+    const { isAuthenticated, isLoading } = useAuth();
+    if (isLoading) return <>Loading...</>;
+
+    if (!isAuthenticated) return <Navigate to="/login" replace />;
     return children;
 };
 
