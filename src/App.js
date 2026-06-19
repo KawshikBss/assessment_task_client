8aa304143b69750b47a7331dc33c08c17a05df41
KawshikBss <kawshikbss@gmail.com>
2026-06-19 23:36:38 +0600

    added necessary providers

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import ClientProvider from "./api/ClientProvider";
+import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <ClientProvider>
+            <AuthProvider>
+                <RouterProvider router={router} />
+                <ToastContainer />
+            </AuthProvider>
+        </ClientProvider>
+    );
 }
 
 export default App;
