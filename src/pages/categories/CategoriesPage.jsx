bff166ec8dee2b29605d27fbce69aa72296fab2c
KawshikBss <kawshikbss@gmail.com>
2026-06-20 01:53:38 +0600

    completed

--- src/pages/categories/CategoriesPage.jsx
+++ src/pages/categories/CategoriesPage.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCategories } from "../../hooks/categories/useCategories";
+import { useDeleteCategory } from "../../hooks/categories/useDeleteCategory";
+import { Button, Table } from "react-bootstrap";
+import { toast } from "react-toastify";
+import CreateUpdateCategoryModal from "../../components/categories/CreateUpdateCategoryModal";
 
 const CategoriesPage = () => {
-    return <div>CategoriesPage</div>;
+    const { data, isLoading, isSuccess, hasNextPage, fetchNextPage } =
+        useCategories();
+
+    const { mutateAsync: deleteCategoryMutation } = useDeleteCategory();
+
+    const onCategoryDelete = async (id) => {
+        const res = await deleteCategoryMutation(id);
+        toast(res.message);
+    };
+
+    const [showModal, setShowModal] = useState(false);
+    const openModal = () => setShowModal(true);
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedCategory(null);
+    };
+
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const onUpdateCategory = (category) => {
+        setSelectedCategory(category);
+        openModal();
+    };
+
+    return (
+        <div className="p-4">
+            {isLoading && !isSuccess ? (
+                <>Loading...</>
+            ) : (
+                <div className="text-center">
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                        <h1>Categories</h1>
+
+                        <Button onClick={openModal}>Create New</Button>
+                    </div>
+                    {data?.pages?.[0]?.data?.total > 0 ? (
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.pages?.map((page) =>
+                                    page?.data?.data?.map((item) => (
+                                        <tr>
+                                            <td>{item.id}</td>
+                                            <td>{item.name}</td>
+                                            <td>
+                                                <Button
+                                                    variant="warning"
+                                                    className="mx-2"
+                                                    onClick={() =>
+                                                        onUpdateCategory(item)
+                                                    }
+                                                >
+                                                    Update
+                                                </Button>
+                                                <Button
+                                                    variant="danger"
+                                                    onClick={() =>
+                                                        onCategoryDelete(
+                                                            item.id,
+                                                        )
+                                                    }
+                                                >
+                                                    Delete
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    )),
+                                )}
+                            </tbody>
+                        </Table>
+                    ) : (
+                        <p>No categories yet</p>
+                    )}
+                    {hasNextPage && (
+                        <Button onClick={fetchNextPage}>More</Button>
+                    )}
+                </div>
+            )}
+            <CreateUpdateCategoryModal
+                category={selectedCategory}
+                show={showModal}
+                handleClose={closeModal}
+            />
+        </div>
+    );
 };
 
 export default CategoriesPage;
--- src/pages/products/ProductsPage.jsx
+++ src/pages/products/ProductsPage.jsx
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { useProducts } from "../../hooks/products/useProducts";
+import { Button, Table } from "react-bootstrap";
+import { useDeleteProduct } from "../../hooks/products/useDeleteProduct";
+import { toast } from "react-toastify";
+import CreateUpdateProductModal from "../../components/products/CreateUpdateProductModal";
 
 const ProductsPage = () => {
-    return <div>ProductsPage</div>;
+    const { data, isLoading, isSuccess, hasNextPage, fetchNextPage } =
+        useProducts();
+
+    const { mutateAsync: deleteProductMutation } = useDeleteProduct();
+
+    const onProductDelete = async (id) => {
+        const res = await deleteProductMutation(id);
+        toast(res.message);
+    };
+
+    const [showModal, setShowModal] = useState(false);
+    const openModal = () => setShowModal(true);
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedProduct(null);
+    };
+
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const onUpdateProduct = (product) => {
+        setSelectedProduct(product);
+        openModal();
+    };
+
+    return (
+        <div className="p-4">
+            {isLoading && !isSuccess ? (
+                <>Loading...</>
+            ) : (
+                <div className="text-center">
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                        <h1>Products</h1>
+
+                        <Button onClick={openModal}>Create New</Button>
+                    </div>
+                    {data?.pages?.[0]?.data?.total > 0 ? (
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.pages?.map((page) =>
+                                    page?.data?.data?.map((item) => (
+                                        <tr>
+                                            <td>{item.id}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.category.name}</td>
+                                            <td>{item.description}</td>
+                                            <td>{item.price}</td>
+                                            <td>
+                                                <Button
+                                                    variant="warning"
+                                                    className="mx-2"
+                                                    onClick={() =>
+                                                        onUpdateProduct(item)
+                                                    }
+                                                >
+                                                    Update
+                                                </Button>
+                                                <Button
+                                                    variant="danger"
+                                                    onClick={() =>
+                                                        onProductDelete(item.id)
+                                                    }
+                                                >
+                                                    Delete
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    )),
+                                )}
+                            </tbody>
+                        </Table>
+                    ) : (
+                        <p>No products yet</p>
+                    )}
+                    {hasNextPage && (
+                        <Button onClick={fetchNextPage}>More</Button>
+                    )}
+                </div>
+            )}
+            <CreateUpdateProductModal
+                product={selectedProduct}
+                show={showModal}
+                handleClose={closeModal}
+            />
+        </div>
+    );
 };
 
 export default ProductsPage;
