f31fae6368b6af40a64430e0703f90651e8961e7
KawshikBss <kawshikbss@gmail.com>
2026-06-19 22:10:56 +0600

    completed design

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    return (
+        <Form className="text-center p-4 rounded-4 shadow w-25">
+            <h1 className="mb-4">Login</h1>
+            <Form.Group className="mb-3 text-start" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 text-start" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+
+            <p className="mt-4">
+                Don't have an account?{" "}
+                <Link className="text-decoration-none" to={"/register"}>
+                    Register
+                </Link>
+            </p>
+        </Form>
+    );
 };
 
 export default LoginPage;
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
-    return <div>RegisterPage</div>;
+    return (
+        <Form className="text-center p-4 rounded-4 shadow w-25">
+            <h1 className="mb-4">Register</h1>
+            <Form.Group className="mb-3 text-start" controlId="name">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 text-start" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 text-start" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group
+                className="mb-3 text-start"
+                controlId="password_confirmation"
+            >
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Register
+            </Button>
+
+            <p className="mt-4">
+                Already have an account?{" "}
+                <Link className="text-decoration-none" to={"/login"}>
+                    Login
+                </Link>
+            </p>
+        </Form>
+    );
 };
 
 export default RegisterPage;
