fd4e543753b7bfa6baab5c80805051b17721c434
KawshikBss <kawshikbss@gmail.com>
2026-06-19 22:13:23 +0600

    completed design

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center">
+        <div className="d-flex vh-100 justify-content-center align-items-center">
             <Outlet />
         </div>
     );
