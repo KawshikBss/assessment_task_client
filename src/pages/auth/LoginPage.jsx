a4b99ce44f6b982eb52a32208eadf295f4634b6f
KawshikBss <kawshikbss@gmail.com>
2026-06-19 23:37:07 +0600

    completed

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,19 +1,60 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
+    const { login } = useAuth();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
+    const navigate = useNavigate();
+
+    const onLogin = async (data) => {
+        const response = await login(data);
+        toast(response.message);
+        navigate("/");
+    };
+
     return (
-        <Form className="text-center p-4 rounded-4 shadow w-25">
+        <Form
+            onSubmit={handleSubmit(onLogin)}
+            className="text-center p-4 rounded-4 shadow w-25"
+        >
             <h1 className="mb-4">Login</h1>
             <Form.Group className="mb-3 text-start" controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    {...register("email", {
+                        required: "Email is required",
+                    })}
+                />
+                {errors.email && (
+                    <p className="text-danger">{errors.email.message}</p>
+                )}
             </Form.Group>
 
             <Form.Group className="mb-3 text-start" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    {...register("password", {
+                        required: "Password is required",
+                        minLength: 8,
+                    })}
+                />
+                {errors.password && (
+                    <p className="text-danger">{errors.password.message}</p>
+                )}
             </Form.Group>
 
             <Button variant="primary" type="submit">
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,24 +1,72 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const RegisterPage = () => {
+    const { register: authRegister } = useAuth();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
+    const navigate = useNavigate();
+
+    const onRegister = async (data) => {
+        const response = await authRegister(data);
+        toast(response.message);
+        navigate("/");
+    };
     return (
-        <Form className="text-center p-4 rounded-4 shadow w-25">
+        <Form
+            onSubmit={handleSubmit(onRegister)}
+            className="text-center p-4 rounded-4 shadow w-25"
+        >
             <h1 className="mb-4">Register</h1>
             <Form.Group className="mb-3 text-start" controlId="name">
                 <Form.Label>Full Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter name" />
+                <Form.Control
+                    type="name"
+                    placeholder="Enter name"
+                    {...register("name", {
+                        required: "Name is required",
+                    })}
+                />
+                {errors.name && (
+                    <p className="text-danger">{errors.name.message}</p>
+                )}
             </Form.Group>
 
             <Form.Group className="mb-3 text-start" controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    {...register("email", {
+                        required: "Email is required",
+                    })}
+                />
+                {errors.email && (
+                    <p className="text-danger">{errors.email.message}</p>
+                )}
             </Form.Group>
 
             <Form.Group className="mb-3 text-start" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    {...register("password", {
+                        required: "Password is required",
+                    })}
+                />
+                {errors.password && (
+                    <p className="text-danger">{errors.password.message}</p>
+                )}
             </Form.Group>
 
             <Form.Group
@@ -26,7 +74,18 @@ const RegisterPage = () => {
                 controlId="password_confirmation"
             >
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Control
+                    type="password"
+                    placeholder="Confirm Password"
+                    {...register("password_confirmation", {
+                        required: "Confirming your password is required",
+                    })}
+                />
+                {errors.password_confirmation && (
+                    <p className="text-danger">
+                        {errors.password_confirmation.message}
+                    </p>
+                )}
             </Form.Group>
 
             <Button variant="primary" type="submit">
