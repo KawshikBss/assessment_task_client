cfe58063452b1d7eaa7b078cfaa076dcce46a071
KawshikBss <kawshikbss@gmail.com>
2026-06-19 23:36:57 +0600

    added navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import MainNavbar from "../components/MainNavbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <MainNavbar />
             <Outlet />
         </div>
     );
