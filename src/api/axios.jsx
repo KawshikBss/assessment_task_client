67f1cd9c785634af2db3ad75537fc70aca8bed9b
KawshikBss <kawshikbss@gmail.com>
2026-06-20 02:16:19 +0600

    modified base url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const baseUrl = process.env.REACT_APP_API_BASE || "http://localhost:8000/api";
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: baseUrl,
     headers: {
         Accept: "application/json",
     },
