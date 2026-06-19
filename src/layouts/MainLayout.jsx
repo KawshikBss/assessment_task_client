1c265f2bc19c1d3a261d756005797b3e32993e0d
KawshikBss <kawshikbss@gmail.com>
2026-06-20 01:53:30 +0600

    fixed import

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import MainNavbar from "../components/MainNavbar";
+import MainNavbar from "../components/layout/MainNavbar";
 
 const MainLayout = () => {
     return (
